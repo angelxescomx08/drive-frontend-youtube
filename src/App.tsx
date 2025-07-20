2fd7a1da8ea68a21edaab240313a208482192c9b
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-07-20 11:24:51 -0600

    Agrega configuración de Docker y nuevas dependencias para React Query. Se crea el archivo `docker-compose.yml` para definir servicios y volúmenes. Se actualizan `package.json` y `package-lock.json` para incluir `@tanstack/react-query`, `@tanstack/react-query-devtools` y `@tanstack/eslint-plugin-query`. Se implementa el cliente de consulta en `src/lib/queryClient.ts` y se envuelven los componentes en `src/App.tsx` con `QueryClientProvider` y `ReactQueryDevtools`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import {
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
