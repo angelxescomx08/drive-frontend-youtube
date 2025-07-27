0b1434b5ba739b59a592b49318f27f3680141d59
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-07-27 11:02:15 -0600

    Agrega `react-router` y configura rutas básicas en `src/App.tsx`. Se actualizan `package.json` y `package-lock.json` para incluir la nueva dependencia y se implementa un enrutador con dos rutas: la raíz y una página "About".

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import './App.css'
-import { Button } from './components/ui/button'
 import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { queryClient } from './lib/queryClient'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>
+  }
+]);
 
 function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+      <RouterProvider router={router} />,
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
